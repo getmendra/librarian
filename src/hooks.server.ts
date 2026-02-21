af015dace90128eb12a47572b13befdc6ae29b6e
timo <22354443+tnkuehne@users.noreply.github.com>
2026-02-21 18:13:08 +0100

    dev mode exclude auth

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,11 +1,14 @@
 import type { Handle } from '@sveltejs/kit';
+import { dev } from '$app/environment';
 import { validateAccess } from '$lib/server/auth';
 
 export const handle: Handle = async ({ event, resolve }) => {
-	try {
-		await validateAccess(event.request);
-	} catch {
-		return new Response('Forbidden', { status: 403 });
+	if (!dev) {
+		try {
+			await validateAccess(event.request);
+		} catch {
+			return new Response('Forbidden', { status: 403 });
+		}
 	}
 
 	return resolve(event);
